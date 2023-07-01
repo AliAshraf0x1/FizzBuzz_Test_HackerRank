98a33aa536f54aae3be12b3c5ca0252dae93a8d5
Ali Ashraf <95376706+AliAshraf0x1@users.noreply.github.com>
2023-07-01 19:06:49 +0300

    Add files via upload

---
+++ FizzBuzz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int in;
+int main ()
+{
+    scanf("%d",&in);
+for(int i=1;i<=in;i++)
+{
+if (i%3==0 && i%5!=0){printf("Fizz\n");}
+else if (i%5==0 && i%3!=0){printf("Buzz\n");}
+else if (i%3==0 && i%5==0){printf("FizzBuzz\n");}
+else{printf("%d\n",i);}
+}
+return 0;
+}
